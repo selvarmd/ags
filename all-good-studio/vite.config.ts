70936e64edc0894140c0c22e63994bfa72c35086
Selvakumar Narayanan <selva.narayanan@aspiresys.com>
2025-10-24 19:54:03 +0530

    deployment bugs fixed

--- all-good-studio/vite.config.ts
+++ all-good-studio/vite.config.ts
@@ -1,17 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/ags/',  // Must match GitHub repo name
-  server: {
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "index.html", // copy index.html
+          dest: ".",         // into dist folder
+          rename: "404.html" // rename it
+        }
+      ]
+    })
+  ],
+  base: "/ags/",
+    server: {
     port: 3000,    // Optional
     open: true     // Optional
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),  // For cleaner imports
+      "@": path.resolve(__dirname, "src"),
     },
   },
-})
+});
